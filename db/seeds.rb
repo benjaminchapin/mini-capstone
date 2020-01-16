5b7f4ec078a3bf71d7ae17e05254ec37aa3565f5
ben <benjamin.n.chapin@gmail.com>
2020-01-16 12:23:36 -0600

    adds products from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(
+  name: "shirt",
+  price: "20",
+  image_url: "https://cdn.shopify.com/s/files/1/0918/8376/products/gpk_adambuttonup2_01_2000x.jpg?v=1529554240",
+  description: "Garbage Pail Kids blue button down shirt size L"
+  )
+
+product.save
+
+product = Product.new(
+  name: "towel",
+  price: "10",
+  image_url: "https://i.ebayimg.com/images/g/hUIAAOSwBLlVON5b/s-l300.jpg",
+  description: "Towelie towel"
+  )
+
+product.save
