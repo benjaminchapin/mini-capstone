9d9e5ed829a4e58f02f6e17ce7cb8de5fb7ebd4b
ben <benjamin.n.chapin@gmail.com>
2020-02-27 10:37:39 -0600

    adds products controller

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,75 @@
+class Api::ProductsController < ApplicationController
+
+  before_action :authenticate_admin, except: [:index, :show, :create, :destroy]
+
+  def index
+    @products = Product.all
+
+    if params[:search]
+      @products = @products.where("name iLIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:discount]
+      @products = @products.where("price < ?", 10)
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "asc"
+      @products = @products.order(:price)
+    elsif params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(price: :desc)
+    else
+      @products = @products.order(:id)
+    end
+
+    if params[:category]
+      # find all the products associated with that category
+      category = Category.find_by(name: params[:category])
+      @products = category.products
+    end
+
+    render 'index.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      stock: params[:stock],
+      supplier_id: params[:supplier_id]
+    )
+    if @product.save
+      # happy path
+      render 'show.json.jb'
+    else
+      # sad path
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.stock = params[:stock] || @product.stock
+
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "Product successfully destroyed"}
+  end
+end
